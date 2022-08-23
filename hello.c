9cdea28845c8e13dbf7c5239dc711a8000486b01
Mason <mpadgett2@huskers.unl.edu>
2022-08-23 18:19:31 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Mason Padgett
+ *         mpadgett2@huskers.unl.edu
+ * Date: 2022/8/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  printf("Mason Padgett\n");
+
+  return 0;
+}
